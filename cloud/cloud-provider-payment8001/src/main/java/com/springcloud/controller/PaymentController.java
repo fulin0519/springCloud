b5c1084094134e8de7f5ab1c4ee1878a564d16b6
fulin <fulin@atwasoft.net>
2020-11-15 19:49:43 +0800

    提交eureka代码

---
+++ cloud/cloud-provider-payment8001/src/main/java/com/springcloud/controller/PaymentController.java
@@ -0,0 +1,60 @@
+package com.springcloud.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springcloud.entities.CommonResult;
+import com.springcloud.entities.Payment;
+import com.springcloud.service.PaymentService;
+import com.sun.org.apache.regexp.internal.recompile;
+
+import lombok.extern.log4j.Log4j2;
+import sun.security.jca.ServiceId;
+
+@RestController
+@Log4j2
+public class PaymentController {
+	
+	
+	@Autowired
+	PaymentService paymentService;
+	
+	
+	@RequestMapping("/test")
+	public String test() {
+		return "ncakcnaiocnacnaocmai";
+	}
+	
+	
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment){
+       int result = paymentService.create(payment);
+       log.info("*****插入结果："+result);
+       if (result>0){  //成功
+           return new CommonResult(200,"插入数据库成功",result);
+       }else {
+           return new CommonResult(444,"插入数据库失败",null);
+       }
+    }
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果："+payment);
+        if (payment!=null){  //说明有数据，能查询成功
+            return new CommonResult(200,"查询成功",payment);
+        }else {
+            return new CommonResult(444,"没有对应记录，查询ID："+id,null);
+        }
+    }
+	
+	
+}
